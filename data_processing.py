6dce85f59922998bf84f79f4c9ae2ff9f0fc33a2
ramuautomation <ramu.pallepati@gmail.com>
2024-08-25 22:49:52 +0530

    new function sum added

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,8 @@
 print("adding data processing python code to fraud detection")
+
+a=5
+b=10
+def sum(a,b):
+    c=a+b
+    return a
+print("sum functionality is added to this file and this code is incorrect")

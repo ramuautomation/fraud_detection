5e7d7ba989b54f09695feed424a8e97e1ad9acac
ramuautomation <ramu.pallepati@gmail.com>
2024-08-26 23:39:52 +0530

    added MLs model code

--- ML_Model.py
+++ ML_Model.py
@@ -2,3 +2,12 @@
 
 
 print("adde all ML_Model code here")
+
+def Model1():
+    print("added all ML1 algoritham code here")
+
+def Model2():
+    print("added all ML2 algoritham code here")
+
+def Model3():
+    print("added all ML3 algoritham code here")
